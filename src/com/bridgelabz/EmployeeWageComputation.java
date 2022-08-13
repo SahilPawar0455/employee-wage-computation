4ad1cd672f755a6abcb0041f8cecc316dd7aac8d
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-13 22:49:09 +0530

    ADDED: solving using switch case statement

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,21 +1,24 @@
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
+    public static final int Emp_Is_Present_Full_Time = 1;
+    public static final int Emp_Is_Present_Half_Time = 2;
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation");
-        int empIsPresentFullTime = 1;
-        int empIsPresentHalfTime = 2;
         int perHrRate = 20;
         int employeeHr = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == empIsPresentFullTime) {
-            System.out.println("Employee is Present in Full Time");
-            employeeHr = 8;
-        } else if (empCheck == empIsPresentHalfTime) {
-            System.out.println("Employee is present in Half Time");
-            employeeHr = 4;
-        } else {
-            System.out.println("Employee is Absent");
+        int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck){
+            case Emp_Is_Present_Full_Time:
+                System.out.println("Employee is Present in Full Time");
+                employeeHr = 8;
+                break;
+            case Emp_Is_Present_Half_Time:
+                System.out.println("Employee is present in Half Time");
+                employeeHr = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
         int employeeWage = perHrRate * employeeHr;
         System.out.println("Employee wage is "+employeeWage);
