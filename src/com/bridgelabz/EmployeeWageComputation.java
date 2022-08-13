5d7bdf0ca20044ae4d924575d1a17b33ca847fff
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-13 22:35:56 +0530

    ADDED: add part time employee & wage

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -3,17 +3,21 @@ package com.bridgelabz;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation");
-        int empIsPresent = 1;
+        int empIsPresentFullTime = 1;
+        int empIsPresentHalfTime = 2;
         int perHrRate = 20;
-        int fullDayHr = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == empIsPresent) {
-            System.out.println("Employee is Present");
-            fullDayHr = 8;
+        int employeeHr = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == empIsPresentFullTime) {
+            System.out.println("Employee is Present in Full Time");
+            employeeHr = 8;
+        } else if (empCheck == empIsPresentHalfTime) {
+            System.out.println("Employee is present in Half Time");
+            employeeHr = 4;
         } else {
             System.out.println("Employee is Absent");
         }
-        int employeeWage = perHrRate * fullDayHr;
+        int employeeWage = perHrRate * employeeHr;
         System.out.println("Employee wage is "+employeeWage);
     }
 }
