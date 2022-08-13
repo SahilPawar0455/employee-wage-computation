774471f02b2c46a138e937b6e9adef3cf0ed4d0c
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-13 23:13:45 +0530

    ADDED: calculate wages till a condition of total working hr or days is reached for a month

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,13 +4,17 @@ public class EmployeeWageComputation {
     public static final int Emp_Is_Present_Full_Time = 1;
     public static final int Emp_Is_Present_Half_Time = 2;
     public static final int Working_Day_In_Month = 20;
+    public static final int Max_Hr_In_Month = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation");
         int perHrRate = 20;
         int employeeHr = 0;
         int totalEmployeeWage = 0;
-        for (int i = 0; i < Working_Day_In_Month; i++) {
+        int totalEmployeeHr = 0;
+        int totalWorkingDay = 0;
+        while (Working_Day_In_Month > totalWorkingDay && (Max_Hr_In_Month >= totalEmployeeHr)) {
+            totalWorkingDay++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case Emp_Is_Present_Full_Time:
@@ -28,7 +32,9 @@ public class EmployeeWageComputation {
             int employeeWage = perHrRate * employeeHr;
             System.out.println("Employee wage is " + employeeWage);
             totalEmployeeWage += employeeWage;
+            totalEmployeeHr += employeeHr;
         }
+        System.out.println("Employee worked hr in month is : "+totalEmployeeHr);
         System.out.println("Total Wage of Month is : " + totalEmployeeWage);
     }
 }
