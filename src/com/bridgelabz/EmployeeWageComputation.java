6643b7ee8231138968b28eb02065e8df86c660bf
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-19 19:31:49 +0530

    ADDED: Refactor the code to write a class method to compute employee wage

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,27 +1,30 @@
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
-    public static final int Emp_Is_Present_Full_Time = 1;
-    public static final int Emp_Is_Present_Half_Time = 2;
-    public static final int Working_Day_In_Month = 20;
-    public static final int Max_Hr_In_Month = 100;
+    public static final int EMP_IS_PRESENT_FULL_TIME = 1;
+    public static final int EMP_IS_PRESENT_HALF_TIME = 2;
+    public static final int WORKING_DAY_IN_MONTH = 20;
+    public static final int MAX_HR_IN_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation");
+        computeEmployeeWage();
+    }
+    public static void computeEmployeeWage(){
         int perHrRate = 20;
         int employeeHr = 0;
         int totalEmployeeWage = 0;
         int totalEmployeeHr = 0;
         int totalWorkingDay = 0;
-        while (Working_Day_In_Month > totalWorkingDay && (Max_Hr_In_Month >= totalEmployeeHr)) {
+        while (WORKING_DAY_IN_MONTH > totalWorkingDay && (MAX_HR_IN_MONTH >= totalEmployeeHr)) {
             totalWorkingDay++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
-                case Emp_Is_Present_Full_Time:
+                case EMP_IS_PRESENT_FULL_TIME:
                     System.out.println("Employee is Present in Full Time");
                     employeeHr = 8;
                     break;
-                case Emp_Is_Present_Half_Time:
+                case EMP_IS_PRESENT_HALF_TIME:
                     System.out.println("Employee is present in Half Time");
                     employeeHr = 4;
                     break;
@@ -34,7 +37,7 @@ public class EmployeeWageComputation {
             totalEmployeeWage += employeeWage;
             totalEmployeeHr += employeeHr;
         }
-        System.out.println("Employee worked hr in month is : "+totalEmployeeHr);
+        System.out.println("Employee worked hr in month is : " + totalEmployeeHr);
         System.out.println("Total Wage of Month is : " + totalEmployeeWage);
     }
 }
